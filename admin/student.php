c64c4c15acf7da9300cbe27a1cfd2065728ffe81
PenunggangUnta <144757634+Darubgt@users.noreply.github.com>
2023-12-13 15:57:04 +0700

    Update student.php

--- admin/student.php
+++ admin/student.php
@@ -33,20 +33,18 @@ if ($_SESSION['RollNo']) {
                                 <b class="caret"></b></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="index.php">Your Profile</a></li>
-                                    <!--li><a href="#">Edit Profile</a></li>
-                                    <li><a href="#">Account Settings</a></li-->
+
                                     <li class="divider"></li>
                                     <li><a href="logout.php">Logout</a></li>
                                 </ul>
                             </li>
                         </ul>
                     </div>
-                    <!-- /.nav-collapse -->
+            
                 </div>
-            </div>
-            <!-- /navbar-inner -->
+    
         </div>
-        <!-- /navbar -->
+   
         <div class="wrapper">
             <div class="container">
                 <div class="row">
@@ -69,9 +67,9 @@ if ($_SESSION['RollNo']) {
                                 <li><a href="logout.php"><i class="menu-icon icon-signout"></i>Logout </a></li>
                             </ul>
                         </div>
-                        <!--/.sidebar-->
+                     
                     </div>
-                    <!--/.span3-->
+
 
                     <div class="span9">
                         <form class="form-horizontal row-fluid" action="student.php" method="post">
@@ -114,7 +112,6 @@ if ($_SESSION['RollNo']) {
                                   <tbody>
                     <?php
                             
-                            //$result=$conn->query($sql);
                             while($row=$result->fetch_assoc())
                             {
 
@@ -129,7 +126,7 @@ if ($_SESSION['RollNo']) {
                                         <td>
                                         <center>
                                             <a href="studentdetails.php?id=<?php echo $rollno; ?>" class="btn btn-success">Details</a>
-                                            <!--a href="remove_student.php?id=<?php echo $rollno; ?>" class="btn btn-danger">Remove</a-->
+                                            <?php echo $rollno; ?>
                                       </center>
                                         </td>
                                     </tr>
@@ -137,10 +134,10 @@ if ($_SESSION['RollNo']) {
                                   </tbody>
                                 </table>
                             </div>
-                    <!--/.span9-->
+                 
                 </div>
             </div>
-            <!--/.container-->
+           
         </div>
 <div class="footer">
             <div class="container">
@@ -148,7 +145,7 @@ if ($_SESSION['RollNo']) {
             </div>
         </div>
         
-        <!--/.wrapper-->
+     
         <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
         <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
         <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
