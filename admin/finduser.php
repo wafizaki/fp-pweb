e694430b0ca7feaa7e8abbe7e94ed7e3d2e46792
PenunggangUnta <144757634+Darubgt@users.noreply.github.com>
2023-12-13 15:51:31 +0700

    Update finduser.php

--- admin/finduser.php
+++ admin/finduser.php
@@ -33,20 +33,16 @@ if ($_SESSION['RollNo']) {
                                 <b class="caret"></b></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="index.php">Your Profile</a></li>
-                                    <!--li><a href="#">Edit Profile</a></li>
-                                    <li><a href="#">Account Settings</a></li-->
                                     <li class="divider"></li>
                                     <li><a href="logout.php">Logout</a></li>
                                 </ul>
                             </li>
                         </ul>
                     </div>
-                    <!-- /.nav-collapse -->
                 </div>
             </div>
-            <!-- /navbar-inner -->
         </div>
-        <!-- /navbar -->
+
         <div class="wrapper">
             <div class="container">
                 <div class="row">
@@ -69,9 +65,8 @@ if ($_SESSION['RollNo']) {
                                 <li><a href="logout.php"><i class="menu-icon icon-signout"></i>Logout </a></li>
                             </ul>
                         </div>
-                        <!--/.sidebar-->
                     </div>
-                    <!--/.span3-->
+
                     <div class="span9">
                         <div class="content">
                             <div class="btn-controls">
@@ -82,14 +77,13 @@ if ($_SESSION['RollNo']) {
                                                                            </a>
                                 </div>
                                 </div>
-                            <!--/.module-->
                         </div>
-                        <!--/.content-->
+
                     </div>
-                    <!--/.span9-->
+
                 </div>
             </div>
-            <!--/.container-->
+
         </div>
 <div class="footer">
             <div class="container">
@@ -97,7 +91,7 @@ if ($_SESSION['RollNo']) {
             </div>
         </div>
         
-        <!--/.wrapper-->
+
         <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
         <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
         <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
