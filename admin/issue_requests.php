86ec3199514da40f6519a3c19c5f27bba19e5040
PenunggangUnta <144757634+Darubgt@users.noreply.github.com>
2023-12-13 15:52:45 +0700

    Update issue_requests.php

--- admin/issue_requests.php
+++ admin/issue_requests.php
@@ -33,20 +33,18 @@ if ($_SESSION['RollNo']) {
                                 <b class="caret"></b></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="index.php">Your Profile</a></li>
-                                    <!--li><a href="#">Edit Profile</a></li>
-                                    <li><a href="#">Account Settings</a></li-->
                                     <li class="divider"></li>
                                     <li><a href="logout.php">Logout</a></li>
                                 </ul>
                             </li>
                         </ul>
                     </div>
-                    <!-- /.nav-collapse -->
+
                 </div>
             </div>
-            <!-- /navbar-inner -->
+      
         </div>
-        <!-- /navbar -->
+   
         <div class="wrapper">
             <div class="container">
                 <div class="row">
@@ -69,7 +67,7 @@ if ($_SESSION['RollNo']) {
                                 <li><a href="logout.php"><i class="menu-icon icon-signout"></i>Logout </a></li>
                             </ul>
                         </div>
-                        <!--/.sidebar-->
+                       
                     </div>
                     <div class="span9">
                         <center>
@@ -118,11 +116,10 @@ if ($_SESSION['RollNo']) {
                                </tbody>
                                 </table>
                             </div>
-                    <!--/.span3-->
-                    <!--/.span9-->
+           
                 </div>
             </div>
-            <!--/.container-->
+         
         </div>
 <div class="footer">
             <div class="container">
@@ -130,7 +127,7 @@ if ($_SESSION['RollNo']) {
             </div>
         </div>
         
-        <!--/.wrapper-->
+   
         <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
         <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
         <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
