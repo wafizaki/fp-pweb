9b81d4a6f1a9032d6fbec29a34e3d6e5bf998e11
KMZeeee <144321137+KMZeeee@users.noreply.github.com>
2023-12-13 15:48:35 +0700

    Update dbconn.php

--- student/dbconn.php
+++ student/dbconn.php
@@ -4,9 +4,7 @@ $dbservername = "localhost";
 $dbusername = "root";
 $dbpassword = "";
 $database = "fp_pweb";
-// Create connection
 $conn = mysqli_connect($dbservername, $dbusername, $dbpassword,$database);
-// Check connection
 if (!$conn) {
     echo "Connected unsuccessfully";
     die("Connection failed: " . mysqli_connect_error());
