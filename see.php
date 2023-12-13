690d513c855253d14789ab19fd38a3922f0cddb3
PenunggangUnta <144757634+Darubgt@users.noreply.github.com>
2023-12-13 15:58:13 +0700

    Update see.php

--- see.php
+++ see.php
@@ -9,18 +9,6 @@ if(isset($_POST['submit']))
 
 $sql1="insert into recommendations (Book_Name,Description,RollNo) values ('$title','$Description','$rollno')"; 
 
-echo $rollno;
-
-/*
-if($conn->query($sql1) === TRUE){
-
-
-echo "<script type='text/javascript'>alert('Success')</script>";
-}
-else
-{//echo $conn->error;
-echo "<script type='text/javascript'>alert('Error')</script>";
-}*/
-    
+echo $rollno;    
 }
 ?> 
