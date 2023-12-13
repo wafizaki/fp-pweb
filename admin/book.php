81ffd935b6392492f81b4576a2dea4ab06c3212d
PenunggangUnta <144757634+Darubgt@users.noreply.github.com>
2023-12-13 15:46:14 +0700

    Update book.php

--- admin/book.php
+++ admin/book.php
@@ -33,20 +33,15 @@ if ($_SESSION['RollNo']) {
                                 <b class="caret"></b></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="index.php">Your Profile</a></li>
-                                    <!--li><a href="#">Edit Profile</a></li>
-                                    <li><a href="#">Account Settings</a></li-->
                                     <li class="divider"></li>
                                     <li><a href="logout.php">Logout</a></li>
                                 </ul>
                             </li>
                         </ul>
                     </div>
-                    <!-- /.nav-collapse -->
                 </div>
             </div>
-            <!-- /navbar-inner -->
         </div>
-        <!-- /navbar -->
         <div class="wrapper">
             <div class="container">
                 <div class="row">
@@ -69,7 +64,6 @@ if ($_SESSION['RollNo']) {
                                 <li><a href="logout.php"><i class="menu-icon icon-signout"></i>Logout </a></li>
                             </ul>
                         </div>
-                        <!--/.sidebar-->
                     </div>
 
                     <div class="span9">
@@ -113,7 +107,6 @@ if ($_SESSION['RollNo']) {
                                   <tbody>
                                     <?php
                             
-                            //$result=$conn->query($sql);
                             while($row=$result->fetch_assoc())
                             {
                                 $bookid=$row['BookId'];
@@ -135,18 +128,15 @@ if ($_SESSION['RollNo']) {
                                </tbody>
                                 </table>
                             </div>
-                    <!--/.span9-->
                 </div>
             </div>
-            <!--/.container-->
         </div>
 <div class="footer">
             <div class="container">
                 <b class="copyright">&copy; 2018 Library Management System </b>All rights reserved.
             </div>
         </div>
-        
-        <!--/.wrapper-->
+
         <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
         <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
         <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
